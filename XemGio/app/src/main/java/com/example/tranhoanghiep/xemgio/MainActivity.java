adde804ea54a835b73a4a9d8ae848f1ea1f6611b
tranduynguyen95 <silverlance_thelegend_2010@yahoo.com>
2016-06-01 15:58:52 +0700

    Them layout
    them layout

--- XemGio/app/src/main/java/com/example/tranhoanghiep/xemgio/MainActivity.java
+++ XemGio/app/src/main/java/com/example/tranhoanghiep/xemgio/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.tranhoanghiep.xemgio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnswitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnswitch = (Button) findViewById(R.id.buttonswitch);
+        btnswitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
